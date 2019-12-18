c164430e922f5462a2e6492faef998cb4dc575e3
peterennis <peterennis@yahoo.com>
2019-12-17 20:59:15 -0800

    wip(test): component-library-react / npm run build

--- packages/component-library-react/dist/components.d.ts
+++ packages/component-library-react/dist/components.d.ts
@@ -1,3 +1,2 @@
 /// <reference types="react" />
-import { JSX } from 'component-library';
-export declare const DemoComponent: import("react").ForwardRefExoticComponent<Pick<import("./react-component-lib/utils").IonicReactExternalProps<JSX.DemoComponent & import("./react-component-lib/ReactProps").ReactProps, HTMLDemoComponentElement>, "max" | "children" | "className" | "advanced" | "min" | "onSlideChanged" | "value"> & import("react").RefAttributes<HTMLDemoComponentElement>>;
+export declare const DemoComponent: import("react").ForwardRefExoticComponent<Pick<any, string | number | symbol> & import("react").RefAttributes<any>>;
